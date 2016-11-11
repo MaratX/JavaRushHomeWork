c66987bb1e55be389f15cb770220cb7d86e1e753
HMF <marioX16@mail.ru>
2016-11-11 13:44:53 +0300

    12

--- src/com/javarush/test/level19/lesson08/task05/Solution.java
+++ src/com/javarush/test/level19/lesson08/task05/Solution.java
@@ -15,10 +15,34 @@ it's a text for testing
 it's a text for testing
 */
 
+import java.io.*;
+
 public class Solution {
     public static TestString testString = new TestString();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
+
+        PrintStream realStream = System.out;
+
+        ByteArrayOutputStream fakeText = new ByteArrayOutputStream();
+
+        PrintStream fakeStream = new PrintStream(fakeText);
+        System.setOut(fakeStream);
+
+        testString.printSomething();
+
+        FileOutputStream fileInputStream = new FileOutputStream(rd.readLine());
+        String fake = fakeText.toString();
+
+        fileInputStream.write(fake.getBytes());
+
+
+        System.setOut(realStream);
+        System.out.println(fake);
+        rd.close();
+        fileInputStream.close();
     }
 
     public static class TestString {
@@ -27,4 +51,3 @@ public class Solution {
         }
     }
 }
-
