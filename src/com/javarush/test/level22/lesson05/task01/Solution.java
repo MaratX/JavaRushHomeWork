318c777b0cd4b7436be613a18fde9387b7b4b4ce
HMF <marioX16@mail.ru>
2016-11-15 08:04:18 +0300

    22.5.2

--- src/com/javarush/test/level22/lesson05/task01/Solution.java
+++ src/com/javarush/test/level22/lesson05/task01/Solution.java
@@ -10,10 +10,43 @@ package com.javarush.test.level22.lesson05.task01;
 Метод main не участвует в тестировании.
 */
 public class Solution {
-    public static String getPartOfString(String string) {
-        return null;
+    public static String getPartOfString(String string) throws TooShortStringException
+    {
+        if (string == null)
+            throw new TooShortStringException();
+        int firstSpace = string.indexOf(" ");
+        if (firstSpace == -1)
+            throw new TooShortStringException();
+        int lastSpace = string.indexOf(" ", firstSpace + 1);
+        if (lastSpace == -1)
+            throw new TooShortStringException();
+        lastSpace = string.indexOf(" ", lastSpace + 1);
+        if (lastSpace == -1)
+            throw new TooShortStringException();
+        lastSpace = string.indexOf(" ", lastSpace + 1);
+        if (lastSpace == -1)
+            throw new TooShortStringException();
+        String afterLastSpace = string.substring(lastSpace + 1);
+        System.out.println(afterLastSpace);
+        char[] afterLastSpaceArray = afterLastSpace.toCharArray();
+        int index = 0;
+        if (!Character.isLetter(afterLastSpaceArray[0]))
+            throw new TooShortStringException();
+        for (int i = 1; i < afterLastSpaceArray.length; i++)
+        {
+            if (Character.isLetter(afterLastSpaceArray[i]))
+                index = i;
+            else
+                break;
+        }
+        return string.substring(firstSpace+1, lastSpace + index + 2);
     }
 
-    public static class TooShortStringException {
+    public static class TooShortStringException extends Exception {
+    }
+
+    public static void main (String[] args) throws TooShortStringException
+    {
+        System.out.println(getPartOfString("JavaRush - лучший сервис обучения Java."));
     }
 }
