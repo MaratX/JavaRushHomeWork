1ac39deda6e4eab64dd3ccee11b8d5e7cd3c1e0b
HMF <marioX16@mail.ru>
2016-11-16 13:16:17 +0300

    22.13.2

--- src/com/javarush/test/level22/lesson13/task01/Solution.java
+++ src/com/javarush/test/level22/lesson13/task01/Solution.java
@@ -1,5 +1,7 @@
 package com.javarush.test.level22.lesson13.task01;
 
+import java.util.StringTokenizer;
+
 /* StringTokenizer
 Используя StringTokenizer разделить query на части по разделителю delimiter.
 Пример,
@@ -7,6 +9,13 @@ getTokens("level22.lesson13.task01", ".") == {"level22", "lesson13", "task01"}
 */
 public class Solution {
     public static String [] getTokens(String query, String delimiter) {
-        return null;
+        StringTokenizer tokenizer = new StringTokenizer(query, delimiter);
+        String[] result = new String[tokenizer.countTokens()];
+        int i = 0;
+        while (tokenizer.hasMoreElements()) {
+            result[i] = tokenizer.nextToken();
+            i++;
+        }
+        return result;
     }
 }
