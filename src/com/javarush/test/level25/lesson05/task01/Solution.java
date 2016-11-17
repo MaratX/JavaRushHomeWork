b68a096284be032c83db7b654f36af2419e7fa76
HMF <marioX16@mail.ru>
2016-11-17 07:28:13 +0300

    25.5.2

--- src/com/javarush/test/level25/lesson05/task01/Solution.java
+++ src/com/javarush/test/level25/lesson05/task01/Solution.java
@@ -10,6 +10,27 @@ package com.javarush.test.level25.lesson05.task01;
 */
 public class Solution {
     public static void processThreads(Thread... threads) {
-        //implement this method - реализуйте этот метод
+        for (Thread currentThread : threads) {
+            switch (currentThread.getState()) {
+                case NEW:
+                    currentThread.start();
+                    break;
+                case RUNNABLE:
+                    currentThread.isInterrupted();
+                    break;
+                case BLOCKED:
+                    currentThread.interrupt();
+                    break;
+                case WAITING:
+                    currentThread.interrupt();
+                    break;
+                case TIMED_WAITING:
+                    currentThread.interrupt();
+                    break;
+                case TERMINATED:
+                    System.out.println(currentThread.getPriority());
+                    break;
+            }
+        }
     }
 }
