fbfc833bcfb2878786f4897a98b6668908cd8d31
HMF <marioX16@mail.ru>
2016-11-18 07:22:15 +0300

    29.5.1

--- src/com/javarush/test/level29/lesson02/task02/Solution.java
+++ src/com/javarush/test/level29/lesson02/task02/Solution.java
@@ -8,23 +8,27 @@ import java.nio.file.Paths;
 Исправить код в соответствии с Naming and Code Convention (Shift+F6 для рефакторинга)
 Не оставлять комментариев, проверяется строгое соответствие стандарту
 */
-public class Solution {
-    public static void main(String[] args) throws IOException, InterruptedException {
-        Solution Solution = new Solution();
-        String file_name_to_be_opened_by_notepad = Solution.Getabsolutepathtodefaulttxtfile().toString();
-        Process NOTEPAD = Solution.getprocessstartnotepad(file_name_to_be_opened_by_notepad);
-        NOTEPAD.waitFor();
+public class Solution
+{
+    public static void main(String[] args) throws IOException, InterruptedException
+    {
+        Solution solution = new Solution();
+        String fileNameToBeOpenedByNotepad = solution.getAbsolutePathToDefaultTxtFile().toString();
+        Process notepad = solution.getProcessStartNotepad(fileNameToBeOpenedByNotepad);
+        notepad.waitFor();
     }
 
-    public Process getprocessstartnotepad(String FILE_NAME) throws IOException {
-        String[] cmd_array = new String[]{"notepad.exe", FILE_NAME};
-        return Runtime.getRuntime().exec(cmd_array);
+    public Process getProcessStartNotepad(String fileName) throws IOException
+    {
+        String[] cmdArray = new String[]{"notepad.exe", fileName};
+        return Runtime.getRuntime().exec(cmdArray);
     }
 
-    public Path Getabsolutepathtodefaulttxtfile() {
-        String PackageName = Solution.class.getPackage().getName().replaceAll("[.]", "\\\\");
-        String FileName = "src\\" + PackageName + "\\file.txt";
-        Path PATH = Paths.get(FileName);
-        return PATH.toAbsolutePath();
+    public Path getAbsolutePathToDefaultTxtFile()
+    {
+        String packageName = Solution.class.getPackage().getName().replaceAll("[.]", "\\\\");
+        String fileName = "src\\" + packageName + "\\file.txt";
+        Path path = Paths.get(fileName);
+        return path.toAbsolutePath();
     }
 }
