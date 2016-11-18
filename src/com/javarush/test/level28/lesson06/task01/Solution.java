49418aeffc8e3e7f7288a263c86639f806bd512a
HMF <marioX16@mail.ru>
2016-11-18 07:12:01 +0300

    28.8.1

--- src/com/javarush/test/level28/lesson06/task01/Solution.java
+++ src/com/javarush/test/level28/lesson06/task01/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.test.level28.lesson06.task01;
 
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.ThreadLocalRandom;
 
 /* Magic class
@@ -11,10 +12,12 @@ import java.util.concurrent.ThreadLocalRandom;
 4. допускаются все элементы включая null
 5. Исправьте строку List<String> list = null ... в методе main
 */
-public class Solution {
-    public static void main(String[] args) throws InterruptedException {
+public class Solution
+{
+    public static void main(String[] args) throws InterruptedException
+    {
         Solution solution = new Solution();
-        List<String> list = null/* create object of this magic class here*/;
+        List<String> list = new CopyOnWriteArrayList<>()/* create object of this magic class here*/;
 
         solution.startUpdatingThread(list);
         solution.copyOnWriteSolution(list);
@@ -28,7 +31,6 @@ Element :s911312405
 Element :s-877520242
 Element :s-1636258097
 size = 5
-
 Element2 :s781859336
 Element2 :s1453499757
 Element2 :s911312405
@@ -41,40 +43,51 @@ size = 8
          */
     }
 
-    public void copyOnWriteSolution(List<String> list) throws InterruptedException {
+    public void copyOnWriteSolution(List<String> list) throws InterruptedException
+    {
         System.out.println("size = " + list.size());
         System.out.println();
         Thread.sleep(20);
-        for (String element : list) {   //создает новую копию на момент создания итератора
+        for (String element : list)
+        {   //создает новую копию на момент создания итератора
             System.out.println("Element :" + element);
         }
         System.out.println("size = " + list.size());
         System.out.println();
         Thread.sleep(20);
 
-        for (String element : list) {  //создает новую копию на момент создания итератора
+        for (String element : list)
+        {  //создает новую копию на момент создания итератора
             System.out.println("Element2 :" + element);
         }
         System.out.println("size = " + list.size());
         stopUpdatingThread();
     }
 
-    public void stopUpdatingThread() throws InterruptedException {
+    public void stopUpdatingThread() throws InterruptedException
+    {
         t.stop();
         t.join();
     }
 
     private Thread t;
 
-    private void startUpdatingThread(final List<String> list) {
-        t = new Thread(new Runnable() {
+    private void startUpdatingThread(final List<String> list)
+    {
+        t = new Thread(new Runnable()
+        {
             @Override
-            public void run() {
-                for (int i = 0; i < 20; i++) {
+            public void run()
+            {
+                for (int i = 0; i < 20; i++)
+                {
                     list.add("s" + ThreadLocalRandom.current().nextInt());
-                    try {
+                    try
+                    {
                         Thread.sleep(5);
-                    } catch (InterruptedException e) {
+                    }
+                    catch (InterruptedException e)
+                    {
                     }
                 }
             }
