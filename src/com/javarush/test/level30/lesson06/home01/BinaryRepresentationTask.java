f668ca93465dfc17ecfc8fd8599d6f391ea99739
HMF <marioX16@mail.ru>
2016-11-22 07:57:15 +0300

    30.8.1

--- src/com/javarush/test/level30/lesson06/home01/BinaryRepresentationTask.java
+++ src/com/javarush/test/level30/lesson06/home01/BinaryRepresentationTask.java
@@ -1,13 +1,30 @@
 package com.javarush.test.level30.lesson06.home01;
 
 import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.RecursiveTask;
 
 /**
  * Created by HMF on 22.11.2016.
  */
-public class BinaryRepresentationTask extends ForkJoinTask<Object>
+public class BinaryRepresentationTask extends RecursiveTask
 {
-    public BinaryRepresentationTask(int i)
-    {
+
+    private int i;
+
+    public BinaryRepresentationTask(int i) {
+        this.i = i;
+    }
+
+    @Override
+    protected String compute() {
+        int a = i % 2;
+        int b = i / 2;
+        String result = String.valueOf(a);
+        if (b > 0) {
+            BinaryRepresentationTask task = new BinaryRepresentationTask(b);
+            task.fork();
+            return task.join() + result;
+        }
+        return result;
     }
 }
--- src/com/javarush/test/level30/lesson06/home01/Solution.java
+++ src/com/javarush/test/level30/lesson06/home01/Solution.java
@@ -10,26 +10,25 @@ import java.util.concurrent.ForkJoinPool;
 4. Пример функциональной реализации - метод binaryRepresentationMethod.
 */
 public class Solution {
-    private String binaryRepresentationMethod(int x) {
+
+    private String binatyRepresentationMethod(int x) {
         int a = x % 2;
         int b = x / 2;
         String result = String.valueOf(a);
         if (b > 0) {
-            return binaryRepresentationMethod(b) + result;
+            return binatyRepresentationMethod(b) + result;
         }
         return result;
     }
 
-
     public static void main(String[] args) {
         Solution solution = new Solution();
-        String result1 = solution.binaryRepresentationMethod(6);
+        String result1 = solution.binatyRepresentationMethod(116);
         System.out.println(result1);
 
         System.out.println();
         ForkJoinPool forkJoinPool = new ForkJoinPool();
-        String result2 = forkJoinPool.invoke(new BinaryRepresentationTask(6));
+        String result2 = (String)forkJoinPool.invoke(new BinaryRepresentationTask(116));
         System.out.println(result2);
     }
-
 }
