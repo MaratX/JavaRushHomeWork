254a8772313deb05ef6b5d819c66da96f6a051b1
HMF <marioX16@mail.ru>
2016-11-22 07:54:46 +0300

    30.6.1

---
+++ src/com/javarush/test/level30/lesson06/home01/BinaryRepresentationTask.java
@@ -0,0 +1,13 @@
+package com.javarush.test.level30.lesson06.home01;
+
+import java.util.concurrent.ForkJoinTask;
+
+/**
+ * Created by HMF on 22.11.2016.
+ */
+public class BinaryRepresentationTask extends ForkJoinTask<Object>
+{
+    public BinaryRepresentationTask(int i)
+    {
+    }
+}
