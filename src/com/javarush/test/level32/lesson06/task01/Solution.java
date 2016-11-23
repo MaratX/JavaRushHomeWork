020c88bee4aec2029ee90753a0cf6324939fc631
HMF <marioX16@mail.ru>
2016-11-23 07:32:30 +0300

    32.8.1h

--- src/com/javarush/test/level32/lesson06/task01/Solution.java
+++ src/com/javarush/test/level32/lesson06/task01/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.test.level32.lesson06.task01;
 
 import java.io.ByteArrayOutputStream;
+import java.util.Random;
 
 /* Генератор паролей
 Реализуйте логику метода getPassword, который должен возвращать ByteArrayOutputStream, в котором будут байты пароля.
@@ -19,6 +20,24 @@ public class Solution {
     }
 
     public static ByteArrayOutputStream getPassword() {
-        return null;
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        Random random = new Random();
+
+        // digits
+        for (int i = 0; i < 3; i++) {
+            byteArrayOutputStream.write(48 + random.nextInt(10));
+        }
+
+        // lower letters
+        for (int i = 0; i < 3; i++) {
+            byteArrayOutputStream.write(65 + random.nextInt(26));
+        }
+
+        // capital letters
+        for (int i = 0; i < 2; i++) {
+            byteArrayOutputStream.write(97 + random.nextInt(26));
+        }
+
+        return byteArrayOutputStream;
     }
 }
