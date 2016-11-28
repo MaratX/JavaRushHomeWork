57b6584768971520973e1e9e390be07ad4c584f1
HMF <marioX16@mail.ru>
2016-11-28 08:35:09 +0300

    34.2.2

--- src/com/javarush/test/level34/lesson02/task03/Solution.java
+++ src/com/javarush/test/level34/lesson02/task03/Solution.java
@@ -12,5 +12,18 @@ package com.javarush.test.level34.lesson02.task03;
 */
 public class Solution {
     public void recursion(int n) {
+        int a = 2;
+        while (a <= n) {
+            if ((n % a) == 0) {
+                if (a != n) {
+                    System.out.print(a + " ");
+                    recursion(n / a);
+                } else {
+                    System.out.print(a);
+                }
+                return;
+            }
+            a++;
+        }
     }
 }
