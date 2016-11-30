58b2830ae54dee84efa3b1f3be1f6f6f78feace3
HMF <marioX16@mail.ru>
2016-11-30 13:17:55 +0300

    35.6.1h

--- src/com/javarush/test/level35/lesson06/task01/Solution.java
+++ src/com/javarush/test/level35/lesson06/task01/Solution.java
@@ -7,18 +7,22 @@ import java.util.LinkedHashMap;
 Параметризируйте класс Solution, чтобы он мог работать со всеми классами, которые наследуются от HashMap.
 Метод getMap должен возвращать тип поля map.
 */
-public class Solution {
+public class Solution<T extends HashMap>
+{
     private HashMap map;
 
-    public Solution(HashMap map) {
+    public Solution(HashMap map)
+    {
         this.map = map;
     }
 
-    public HashMap getMap() {
+    public HashMap getMap()
+    {
         return map;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         HashMap<String, Integer> hashMap = new HashMap<>();
         hashMap.put("string", 4);
         Solution solution = new Solution(hashMap);
@@ -29,7 +33,7 @@ public class Solution {
         LinkedHashMap<Solution, Solution> hashMap2 = new LinkedHashMap<>();
         hashMap2.put(solution, solution);
         Solution solution2 = new Solution(hashMap2);
-        LinkedHashMap mapFromSolution2 = (LinkedHashMap)solution2.getMap();   //need to cast  :(
+        LinkedHashMap mapFromSolution2 = (LinkedHashMap) solution2.getMap();   //need to cast  :(
         System.out.println(mapFromSolution2.getClass());
     }
 }
