eb115c68befff946834865e53428b41a61518636
HMF <marioX16@mail.ru>
2016-11-30 13:15:49 +0300

    35.6.1

--- src/com/javarush/test/level35/lesson08/task01/Solution.java
+++ src/com/javarush/test/level35/lesson08/task01/Solution.java
@@ -8,18 +8,22 @@ import java.util.ListIterator;
 Логику не меняйте.
 Не оставляйте закомментированный код.
 */
-public class Solution {
+public class Solution
+{
 
-    public static <D, H extends D, S extends H> void add(List<D> destinationList, List<S> sourceList) {
-        ListIterator<D> destListIterator = destinationList.listIterator();
-        ListIterator<S> srcListIterator = sourceList.listIterator();
-        for (int i = 0; i < sourceList.size(); i++) {
+    public static <D> void add(List<? super D> destinationList, List<? extends D> sourceList)
+    {
+        ListIterator<? super D> destListIterator = destinationList.listIterator();
+        ListIterator<? extends D> srcListIterator = sourceList.listIterator();
+        for (int i = 0; i < sourceList.size(); i++)
+        {
             destListIterator.add(srcListIterator.next());
         }
     }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         List<B> destination = new ArrayList<>();
         destination.add(new B());
         List<C> source = new ArrayList<>();
@@ -28,18 +32,18 @@ public class Solution {
         System.out.println(destination);
         System.out.println(source);
 
-        /*
-[com.javarush.test.level39.lesson08.task01.Solution$C@203b4f0e, com.javarush.test.level39.lesson08.task01.Solution$B@15c330aa]
-[com.javarush.test.level39.lesson08.task01.Solution$C@203b4f0e]
-         */
+
     }
 
-    static class A {
+    static class A
+    {
     }
 
-    static class B extends A {
+    static class B extends A
+    {
     }
 
-    static class C extends B {
+    static class C extends B
+    {
     }
 }
