f6f463e9fb0fde5ff2a8f44c01115e3943d15f08
HMF <marioX16@mail.ru>
2016-11-30 13:20:10 +0300

    36.4.1

--- src/com/javarush/test/level35/lesson06/home01/Solution.java
+++ src/com/javarush/test/level35/lesson06/home01/Solution.java
@@ -6,21 +6,22 @@ package com.javarush.test.level35.lesson06.home01;
 T должен быть наследником класса ClassForGenerics и одновременно реализовывать интерфейс InterfaceForGenerics.
 Менять можно только класс Solution.
 */
-public class Solution<T> {
+public class Solution<T extends ClassForGenerics & InterfaceForGenerics> {
     public static void main(String[] args) {
         Solution<TestClassGood> testClassSolution = new Solution<>();
         testClassSolution.check();
 
         //!!! Следующие оба варианта не должны работать:
-        Solution<TestClassWrong1> wrong1Solution = new Solution<>();
+        /*Solution<TestClassWrong1> wrong1Solution = new Solution<>();
         wrong1Solution.check();
-
         Solution<TestClassWrong2> wrong2Solution = new Solution<>();
-        wrong2Solution.check();
+        wrong2Solution.check();*/
     }
 
     public void check() {
+
         System.out.println("Works!");
+
     }
 
     public static class TestClassGood extends ClassForGenerics implements InterfaceForGenerics {
@@ -34,5 +35,4 @@ public class Solution<T> {
     public static class TestClassWrong2 implements InterfaceForGenerics {
 
     }
-
 }
