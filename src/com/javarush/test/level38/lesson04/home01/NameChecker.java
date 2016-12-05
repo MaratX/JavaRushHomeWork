cf2e3c4d61e79b8f1c4d6d3901ccafef8e5fa98b
HMF <marioX16@mail.ru>
2016-12-05 15:14:53 +0300

    38.4.1t

--- src/com/javarush/test/level38/lesson04/home01/NameChecker.java
+++ src/com/javarush/test/level38/lesson04/home01/NameChecker.java
@@ -2,7 +2,7 @@ package com.javarush.test.level38.lesson04.home01;
 
 public class NameChecker {
     private static final int MAX_NUMBER_OF_CHARACTERS = 100500;
-    public static int getNumberOfCharacters(String name) throws NameIsEmptyException, NameIsNullException {
+    public static int getNumberOfCharacters(String name) throws Exception {
         if (name == null)
             throw new NameIsNullException();
         else if (name.isEmpty())
